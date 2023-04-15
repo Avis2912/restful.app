13b7dcd6d26d792e7485c87e7f7836017531647f
furqan4545 <furqan.ali9500@gmail.com>
2023-04-15 18:52:33 +0900

    0.1.0

--- background.js
+++ background.js
@@ -20,3 +20,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return true;
   }
 });
+
+
+
+
+
--- content.js
+++ content.js
@@ -1,58 +1,54 @@
-// chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-//   var url = tabs[0].url
-//   console.log(JSON.stringify(url));
-// })
-
 const openAiApiCall = async (apiKey, prompt) => {
-  const response = await fetch("https://api.openai.com/v1/completions", {
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${apiKey}`,
     },
     body: JSON.stringify({
-      prompt: prompt,
-      model: "text-davinci-003",
-      max_tokens: 100,
-      n: 1,
-      stop: null,
-      temperature: 0.7,
+      model: "gpt-3.5-turbo", 
+      messages: [{"role": "user", "content": prompt}],
+      temperature: 0.7
     }),
   });
 
-  
   if (!response.ok) {
     throw new Error(`API call failed: ${response.statusText}`);
   }
 
   const data = await response.json();
-  return data.choices[0].text.trim();
+  console.log("data check: ", data.choices[0].message)
+  return data.choices[0].message.content;
 };
 
 chrome.runtime.sendMessage({ type: "getKeys" }, async (response) => {
   const apiKey = response.apiKey;
 
   if (!apiKey) {
+    alert("Please set your OpenAI API key in the extension options.");
     return;
   }
-  //Loop
 
   document.addEventListener("keyup", async (event) => {
     if (event.key !== ";") {
       return;
     }
-    async function getCurrentTab() {
-      let queryOptions = { active: true, currentWindow: true };
-      let [tab] = await chrome.tabs.query(queryOptions);
-      return tab;
-    }
-    let currentUrl = await getCurrentTab();
 
-    if (currentUrl.includes("linkedin.com")){
-    let inputElement = document.querySelector('.ql-editor');
-    const inputText = inputElement.innerText
+    const target = event.target;
+    // let inputText = target.value;    /// original working code here.
+
+    let inputText;
+    if (target.value !== "" && target.value !== undefined) {
+      const cursorPosition = target.selectionStart;
+      const startOfLine = target.value.lastIndexOf('\n', cursorPosition - 1) + 1;
+      const endOfLine = target.value.indexOf('\n', cursorPosition);
+      inputText = endOfLine === -1 ? target.value.slice(startOfLine) : target.value.slice(startOfLine, endOfLine);
+    } else {
+      inputText = document.activeElement.innerText; // LinkedIn
+      if (inputText === "" || inputText === undefined) {
+        inputText = document.activeElement.textContent; // Twitter
+      }
     }
-    // let inputText = target.value;
 
     // if (inputText === "" || inputText === undefined) {
     //   inputText = document.activeElement.innerText; // LinkedIn
@@ -61,57 +57,201 @@ chrome.runtime.sendMessage({ type: "getKeys" }, async (response) => {
     //   }
     // }
 
-    console.log("inputText: ", inputText);
+    alert("input text: " + inputText);
+    // console.log("inputText: ",ext);
 
-    // Match formats
-    // const helpRegex = /help:\s*(.+?);/g;
-    const langRegex = /translate:\s*(.+?):\s*(.+?);/g;
-    const codeRegex = /code:\s*(.+?):\s*(.+?);/g;
-    //V1
-    const scribRegex = /scrib:\s*(.+?);/g;
-    const summarizeRegex = /summarize:\s*(.+?);/g;
-    const elongateRegex = /elongate:\s*(.+?);/g;
-    const listRegex = /list:\s*(.+?);/g;
-    const toneRegex = /tone:\s*(.+?):\s*(.+?);/g;
-    //End of V1
+    const lastHelpCommandIndex = inputText.lastIndexOf('help:');
+    if (lastHelpCommandIndex >= 0) {
+      inputText = inputText.slice(lastHelpCommandIndex);
+    }
 
-    const updateInput = async (regex, callback) => {
-      let updatedText = inputText;
-      let match;
-      let matchFound = false;
-
-      console.log("regex: ", regex);
-      console.log("updatedText: ", JSON.stringify(updatedText));
-
-      while ((match = regex.exec(updatedText)) !== null) {
-        console.log("Match found:", match); // Log the match
-        matchFound = true;
-        const generatedText = await callback(match);
-        console.log("GeneratedText: ", generatedText);
-        updatedText = updatedText.replace(match[0], generatedText);
-
-        // Reset lastIndex to 0 to avoid infinite loops
-        regex.lastIndex = 0;
+      // Match formats
+      // const helpRegex = /help:(?:(\d*):)?\s*(.+?)(?=;)/g; // Updated regex for help to make the word limit optional
+      // const helpRegex = /(?:^|\n)help:(?:(\d*):)?\s*([^\n]+?)(?=;|\n|$)/g;
+      const helpRegex = /(?:^|[\r\n])help:(?:(\d*):)?\s*([^\r\n]+?)(?=;|$)/g;
+      const helpMatch = inputText.match(helpRegex);
+      if (helpMatch) {
+        inputText = helpMatch[0];
       }
-      if (matchFound) {
-        const event = new Event("input", {
+
+      const langRegex = /translate,([a-zA-Z]+):\s*(.+?);/g;
+      const codeRegex = /code,([a-zA-Z]+):\s*(.+?);/g;
+      const chatgptRegex = /chatgpt:\s*(.+?)(?=;)/g; // Added regex for ChatGPT
+
+      const updateInput = async (regex, callback) => {
+        const matches = Array.from(inputText.matchAll(regex));
+      
+        if (matches.length === 0) {
+          return;
+        }
+    
+        const replacements = await Promise.all(matches.map(match => callback(match)));
+        let updatedText = inputText;
+      
+        for (let i = 0; i < matches.length; i++) {
+          const match = matches[i];
+          const generatedText = replacements[i];
+          // updatedText += "\n\n" + generatedText.replace(match[0], "").trim(); // Append the generated text instead of replacing
+          updatedText = inputText.slice(1, -match[0].length) + "\n\n" + generatedText;
+        }
+        console.log("updatedText: ", updatedText);
+
+        const activeElement = document.activeElement;
+
+        if (activeElement.tagName === "TEXTAREA" || activeElement.tagName === "INPUT") {
+          // For textarea and input elements
+          const event = new Event("keydown", {
+            bubbles: true,
+            cancelable: true,
+            key: "a", // Replace with the key you want to simulate
+            code: "KeyA", // Replace with the key code you want to simulate
+          });
+
+        document.activeElement.dispatchEvent(event);
+      
+
+        // Calculate the difference between original and updated text
+        const difference = updatedText.slice(inputText.length);
+      
+        // Use InputEvent for updating text
+        const inputEvent = new InputEvent("input", {
+          bubbles: true,
+          cancelable: true,
+          data: difference,
+        });
+
+        // Set the value before dispatching the input event
+        document.activeElement.value = updatedText;
+        console.log("Input event created:", updatedText);
+
+        document.activeElement.dispatchEvent(inputEvent);
+        console.log("Input event dispatched");
+
+        // document.activeElement.innerText = updatedText; //Linkedin
+        // document.activeElement.textContent = updatedText; //Twitter
+      } else {
+        // For contentEditable elements
+        const sel = window.getSelection();
+        const range = sel.getRangeAt(0);
+        const endNode = document.createTextNode("\n\n" + updatedText);
+        range.insertNode(endNode);
+        range.setStartAfter(endNode);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+      }
+    };
+
+
+    const isActiveElementContentEditable = () => {
+      return document.activeElement.getAttribute("contenteditable") === "true";
+    };
+
+    const updateInputReddit = async (regex, callback) => {
+      const matches = Array.from(inputText.matchAll(regex));
+    
+      if (matches.length === 0) {
+        return;
+      }
+
+      const replacements = await Promise.all(matches.map(match => callback(match)));
+      let updatedText = inputText;
+    
+      for (let i = 0; i < matches.length; i++) {
+        const match = matches[i];
+        const generatedText = replacements[i];
+        // updatedText += "\n\n" + generatedText.replace(match[0], "").trim(); // Append the generated text instead of replacing
+        updatedText = inputText.slice(1, -match[0].length) + "\n\n" + generatedText;
+      }
+      console.log("updatedText: ", updatedText);
+
+      const activeElement = document.activeElement;
+
+      if (activeElement.tagName === "TEXTAREA" || activeElement.tagName === "INPUT") {
+        // For textarea and input elements
+        const event = new Event("keydown", {
+          bubbles: true,
+          cancelable: true,
+          key: "a", // Replace with the key you want to simulate
+          code: "KeyA", // Replace with the key code you want to simulate
+        });
+
+      document.activeElement.dispatchEvent(event);
+    
+
+      // Calculate the difference between original and updated text
+      const difference = updatedText.slice(inputText.length);
+    
+      // Use InputEvent for updating text
+      const inputEvent = new InputEvent("input", {
+        bubbles: true,
+        cancelable: true,
+        data: difference,
+      });
+
+      // Set the value before dispatching the input event
+      document.activeElement.value = updatedText;
+      console.log("Input event created:", updatedText);
+
+      document.activeElement.dispatchEvent(inputEvent);
+      console.log("Input event dispatched");
+    } else if (isActiveElementContentEditable()) {
+      if (isReddit()) {
+        const sel = window.getSelection();
+        const range = sel.getRangeAt(0);
+        const endNode = document.createTextNode("\n\n" + updatedText);
+        range.insertNode(endNode);
+        range.setStartAfter(endNode);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+
+        // Trigger input event for React
+        const inputEvent = new Event("input", {
           bubbles: true,
           cancelable: true,
         });
-        await document.activeElement.dispatchEvent(event);
-        target.value = updatedText; //General
-        document.activeElement.innerText = updatedText; //Linkedin
-        document.activeElement.textContent = updatedText; //Twitter
+        activeElement.dispatchEvent(inputEvent);
       }
-    };
+      else if (isNotion()) {
+        // For contentEditable elements on Notion
+        const sel = window.getSelection();
+        const range = sel.getRangeAt(0);
+    
+        // Create a new text node with the updatedText and a line break
+        const endNode = document.createTextNode("\n" + updatedText);
+    
+        // Insert the new text node at the current cursor position
+        range.insertNode(endNode);
+    
+        // Set the cursor position after the inserted text node
+        range.setStartAfter(endNode);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    
+        // Trigger input event for Notion's React component
+        const inputEvent = new Event("input", {
+          bubbles: true,
+          cancelable: true,
+        });
+        activeElement.dispatchEvent(inputEvent);
+      } 
+    }
+  };
+    
 
-    const generateText = async (query) => {
-      console.log("query: ", query);
-      return await openAiApiCall(apiKey, query);
+    const generateText = async (query, wordLimit) => {
+      const prompt = wordLimit ? `In exactly : ${wordLimit} words: ${query}` : `help: ${query}`;
+      console.log("prompt: ", prompt);
+
+      const responseText = await openAiApiCall(apiKey, prompt);
+      console.log("response got: "+ responseText);
+      return responseText.replace(prompt, "").trim();
+      // return await openAiApiCall(apiKey, query);
     };
 
     const translateText = async (language, query) => {
-      console.log("language: ", language);
       const prompt = `Translate the following English text to ${language}: "${query}"`;
       return await openAiApiCall(apiKey, prompt);
     };
@@ -122,52 +262,28 @@ chrome.runtime.sendMessage({ type: "getKeys" }, async (response) => {
       return await openAiApiCall(apiKey, prompt);
     };
 
-    //New V1
-    const summarizeText = async (query) => {
-      console.log("summarize: ", query);
-      return await openAiApiCall(apiKey, query);
-    };
-    const elongateText = async (query) => {
-      console.log("elongate: ", query);
-      return await openAiApiCall(apiKey, query);
-    };
-    const listText = async (query) => {
-      console.log("Turn into list: ", query);
-      return await openAiApiCall(apiKey, query);
-    };
-    const toneText = async (tone, query) => {
-      console.log("tone", tone);
-      const prompt = `Write ${tone} tone to ${query}`;
-      return await openAiApiCall(apiKey, prompt);
+    const isReddit = () => {
+      return window.location.hostname.includes("reddit.com");
     };
 
-    await updateInput(summarizeRegex, (match) => summarizeText(match[1]));
-    await updateInput(elongateRegex, (match) => elongateText(match[1]));
-    await updateInput(listRegex, (match) => listText(match[1]));
-    await updateInput(toneRegex, (match) => toneText(match[1], match[2]));
+    const isNotion = () => {
+      return window.location.hostname.includes("notion.so");
+    };
+
+    console.log("help thing:"+ helpRegex);
+
+    if (isReddit() || isNotion()) {
+      await updateInputReddit(helpRegex, (match) => generateText(match[2], match[1] ? parseInt(match[1]) : null));
+      await updateInputReddit(langRegex, (match) => translateText(match[1], match[2]));
+      await updateInputReddit(codeRegex, (match) => generateCode(match[1], match[2]));
+      await updateInputReddit(chatgptRegex, (match) => generateText(match[1])); // Added call for ChatGPT  
+    }
+    else {
+      await updateInput(helpRegex, (match) => generateText(match[2], match[1] ? parseInt(match[1]) : null));
+      await updateInput(langRegex, (match) => translateText(match[1], match[2]));
+      await updateInput(codeRegex, (match) => generateCode(match[1], match[2]));
+      await updateInput(chatgptRegex, (match) => generateText(match[1])); // Added call for ChatGPT
+    }
     
-    //End of V1
-
-    await updateInput(scribRegex, (match) => generateText(match[1]));
-    await updateInput(langRegex, (match) => translateText(match[1], match[2]));
-    await updateInput(codeRegex, (match) => generateCode(match[1], match[2]));
   });
 });
-
-// - 
-
-// /scrib for general prompting
-// /code for code but ideally /code, language name for code
-// /summarize for summarizing
-// /rewrite for rewriting
-// /elongate for making text longer
-
-// /friendly, 
-
-// /formal
-
-// /casual
-
-// /excited
-// /translate, language name for translating
-// /list to convert a para into a list
